7655c8688686fcb986ce8f1915a351ec9004fef2
Vincent Hu <vincent.hu@yale.edu>
2013-11-05 07:08:36 -0500

    Updated tumblr address to nhelection13

--- scripts/script.js
+++ scripts/script.js
@@ -19,7 +19,7 @@ function setHeight(elem1, elem2) {
 var tlog = "";
 
 function lookforchanges() {
-    $.get("http://ydnelections.tumblr.com/js?num=1000", checkdiff);
+    $.get("http://nhelection13.tumblr.com/js?num=1000", checkdiff);
 }
 
 function checkdiff(data) {
